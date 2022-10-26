2aa29dba68d7a3bf49bcd8821a6d321b8ab12943
Nicolas <nicolas@MacBook-Pro-de-Nicolas.local>
2022-10-26 16:28:31 +0200

    add icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.scss";
+
 import { Routes, Route } from "react-router-dom";
+
 import Layout from "./components/Layout";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+library.add(faHouse);
 
 function App() {
   return (
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,11 +1,20 @@
 import "./index.scss";
 
+import { NavLink } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Sidebar = () => (
   <div className="nav-bar">
     <div className="logo">
       <h3 className="logo-initial">NR</h3>
       <p>Nicolas RIERA</p>
     </div>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon="fa-solid fa-house" />
+      </NavLink>
+    </nav>
   </div>
 );
 
