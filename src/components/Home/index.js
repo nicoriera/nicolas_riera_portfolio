021a54e6ccdfb53e008cb2a90de955923f939da0
Nicolas RIERA <nicolariera@gmail.com>
2022-11-23 12:38:47 +0100

    add useeffect animate letter

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./index.scss";
-import { useState } from "react";
 import AnimatedLetters from "../AnimatedLetters";
 
 const Home = () => {
@@ -29,18 +29,36 @@ const Home = () => {
     "r",
   ];
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
           <AnimatedLetters
             letterClass={letterClass}
             strArray={nameArray}
             idx={15}
           />
           <br />
-          junior web developer
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={23}
+          />
         </h1>
         <h2>Junior Developer Web / Javascript / React</h2>
         <Link to="/contact" className="flat-button">
