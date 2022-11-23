4ce1ef2e9761cc0cfac959ddaccf37fa9801ac08
Nicolas RIERA <nicolariera@gmail.com>
2022-11-23 11:14:08 +0100

    add component animatedletters in component home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,44 @@
 import { Link } from "react-router-dom";
 import "./index.scss";
+import { useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = [" ", "N", "i", "c", "o", "l", "a", "s"];
+  const jobArray = [
+    "j",
+    "u",
+    "n",
+    "i",
+    "o",
+    "r",
+    " ",
+    "w",
+    "e",
+    "b",
+    " ",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+  ];
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm Nicolas
+          Hi, <br /> I'm
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
           <br />
           junior web developer
         </h1>
