7525c87fd8c4a49aa68f77b74f47ab95e76ec56e
Nicolas RIERA <nicolariera@gmail.com>
2022-11-28 15:59:35 +0100

    create page contact and form

---
+++ src/components/Contact/index.js
@@ -0,0 +1,76 @@
+import "./index.scss";
+import { Loader } from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+import { useEffect, useState } from "react";
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const contactArray = ["C", "o", "n", "t", "a", "c", "t", " ", "m", "e"];
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={contactArray}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in opportunities. However, if you have other request
+            or question, don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form action="">
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    nam="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
+};
+
+export default Contact;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./index.scss";
 import AnimatedLetters from "../AnimatedLetters";
+import { Loader } from "react-loaders";
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
@@ -41,32 +42,35 @@ const Home = () => {
   });
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={23}
-          />
-        </h1>
-        <h2>Junior Developer Web / Javascript / React</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={23}
+            />
+          </h1>
+          <h2>Junior Developer Web / Javascript / React</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
